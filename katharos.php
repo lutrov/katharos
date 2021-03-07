9ca5b33c2c9d7f47c6b852d03ce15301386b2be1
Ivan Lutrov <lutrov@users.noreply.github.com>
2021-03-07 14:40:17 +1100

    Update katharos.php

--- katharos.php
+++ katharos.php
@@ -3,11 +3,25 @@
 /*
 Plugin Name: Katharos
 Description: Reduces the amount of bandwidth your site and your visitor uses by using sophisticated output buffering techniques to clean and compress your site's webpage content before it gets sent to the user's browser. Why this plugin name? Katharos means "pure" in Greek.
+Version: 5.1
 Plugin URI: https://github.com/lutrov/katharos
 Author: Ivan Lutrov
 Author URI: http://lutrov.com/
-Version: 5.1
-Notes: This plugin provides an API to customise the default constant values. See the "readme.md" file for more.
+Copyright: 2014, Ivan Lutrov
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+Street, Fifth Floor, Boston, MA 02110-1301, USA. Also add information on how to
+contact you by electronic and paper mail.
 */
 
 defined('ABSPATH') || die('Ahem.');
